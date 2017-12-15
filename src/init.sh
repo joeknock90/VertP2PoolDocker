4afbdf66fd6cc0db577bddaf2ac7418a359c8d1a
Joseph Knockenhauer <jknockenhauer@mfbcpa.com>
2017-12-15 16:28:49 -0500

    change to /root temporarily to get it working

--- src/init.sh
+++ src/init.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # generate vertcoin.conf
-cat <<EOF > /config/vertcoin.conf
+cat <<EOF > /root/.vertcoin/vertcoin.conf
   server=1
   rpcuser=$RPC_USER
   rpcpassword=$RPC_PASSWORD
 EOF
 
-python /p2pool-vtc/run_p2pool.py --datadir /data -f $FEE --bitcoind-address $VERTCOIND_HOST --bitcoind-rpc-port $VERTCOIND_HOST_PORT --max-conns $MAX_CONNECTIONS -a $FEE_ADDRESS
+python /p2pool-vtc/run_p2pool.py --net vertcoin --datadir /data -f $FEE --bitcoind-address $VERTCOIND_HOST --bitcoind-rpc-port $VERTCOIND_HOST_PORT --max-conns $MAX_CONNECTIONS -a $FEE_ADDRESS
