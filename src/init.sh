3410c84f4e4d6cb1eb48e39f88febd2433bc8e56
Joseph Knockenhauer <jknockenhauer@mfbcpa.com>
2017-12-15 16:55:29 -0500

    typo on init command

--- src/init.sh
+++ src/init.sh
@@ -12,4 +12,4 @@ cat <<EOF > /root/.vertcoin/vertcoin.conf
   rpcpassword=$RPC_PASSWORD
 EOF
 
-python /p2pool-vtc/run_p2pool.py --net $NET --datadir /data -f $FEE --bitcoind-address $VERTCOIND_HOST --bitcoind-rpc-port $VERTCOIND_HOST_PORT --max-conns $MAX_CONNECTIONS -a $FEE_ADDRESS -p2pool-port 9346 --worker-port 9171
+python /p2pool-vtc/run_p2pool.py --net $NET --datadir /data -f $FEE --bitcoind-address $VERTCOIND_HOST --bitcoind-rpc-port $VERTCOIND_HOST_PORT --max-conns $MAX_CONNECTIONS -a $FEE_ADDRESS --p2pool-port 9346 --worker-port 9171
